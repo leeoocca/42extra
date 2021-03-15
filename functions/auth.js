45c82cad3c89ee85bef6eb1a7194ade834bf9900
leeoocca <leeoocca@gmail.com>
2021-03-15 20:38:38 +0100

    - added auth serverless functions - cleaned readme.md - removed unneeded cra files

---
+++ functions/auth.js
@@ -0,0 +1,32 @@
+const oauth = require("./utils/oauth");
+
+exports.handler = async (event) => {
+	if (!event.queryStringParameters) {
+		return {
+			statusCode: 401,
+			body: JSON.stringify({
+				error: "Missing required parameters `url` and `csrf`",
+			}),
+		};
+	}
+
+	const { csrf: csrfToken, url } = event.queryStringParameters;
+
+	const authorizationURI = oauth.authorizeURL({
+		redirect_uri: process.env.FT_REDIRECT_URI,
+		state: `url=${url}&csrf=${csrfToken}`,
+
+		// for now, this is always blank. in the future, specific scopes will be
+		// required to perform actions on the user’s behalf
+		scope: "",
+	});
+
+	return {
+		statusCode: 302,
+		headers: {
+			Location: authorizationURI,
+			"Cache-Control": "no-cache",
+		},
+		body: "redirecting to authorization...",
+	};
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Welcome!</p>
+				<a
+					className="App-link"
+					href={
+						"https://api.intra.42.fr/oauth/authorize?\
+						client_id=b60505384faf7b8305c22e79f1c16e7593c8968a8a0d2d430e01d4b82122490c\
+						&redirect_uri=https%3A%2F%2F42react.netlify.app%2F.netlify%2Ffunctions%2Fauth-callback\
+						&response_type=code"
+					}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Login
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
