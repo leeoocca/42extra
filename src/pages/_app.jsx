4ac6aebd77354c67de78ac59847fc0f6c8130c4d
leeoocca <leeoocca@gmail.com>
2021-04-29 00:26:33 +0200

    disable swr revalidateOnFocus

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
 				onError: (err) => {
 					console.error(err);
 				},
+				revalidateOnFocus: false,
 			}}
 		>
 			<Provider session={pageProps.session}>
