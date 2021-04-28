d752056026f136e3d154ea261928a920e1f8f3d8
leeoocca <leeoocca@gmail.com>
2021-04-28 15:39:32 +0200

    scroll into view current nav item

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,18 +1,30 @@
 import Link from "next/link";
 import { LinkListNode } from "@interfaces/LinkListNode";
 import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
 
 function NavLink({ name, href, className }: LinkListNode) {
 	const router = useRouter();
 	const { login } = router.query;
+	const activeNavRef = useRef<HTMLAnchorElement>(null);
+
+	const active = router.pathname === href;
+
+	useEffect(() => {
+		if (active && activeNavRef.current) {
+			activeNavRef.current.scrollIntoView();
+		}
+	}, []);
+
 	return (
 		<Link href={{ pathname: href, query: { login: login } }} key={href}>
 			<a
 				className={`${
-					router.pathname === href
+					active
 						? "opacity-100 border-white border-b-2"
 						: "opacity-75"
 				} hover:opacity-100 hover:text-white px-3 py-2 font-medium ease-in-out transition-opacity focus:opacity-100 ${className}`}
+				ref={activeNavRef}
 			>
 				{name}
 			</a>
