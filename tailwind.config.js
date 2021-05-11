5bd48917c6c3f7dbbcbd3700fc8fb55770de5965
leeoocca <leeoocca@gmail.com>
2021-05-12 00:06:32 +0200

    tailwind: inter font, forms
    also
    - upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	mode: "jit",
 	purge: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+			},
 			textColor: {
 				skin: {
 					base: "var(--color-base)",
@@ -21,5 +26,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/forms")],
 };
