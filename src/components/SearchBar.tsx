9fa93f3fde032944f289e0ff4a8ca91a08672494
leeoocca <leeoocca@gmail.com>
2021-05-12 02:51:51 +0200

    new `StatusBar` design, with WIP `SearchBar`

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const links = [
+	{ name: "users", href: "/users" },
+	{ name: "projects", href: "/projects" },
+];
+
+function SearchBar() {
+	const [search, setSearch] = useState("");
+	const [visibility, setVisibility] = useState(false);
+	const router = useRouter();
+	return (
+		<>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					// while (search.length && search[0] == "/")
+					// 	setSearch(search.substr(1));
+					router.push(`/${search}`, null, { shallow: true });
+					// https://stackoverflow.com/a/56899483/14477874
+					if (document.activeElement instanceof HTMLElement)
+						document.activeElement.blur();
+					setSearch("");
+				}}
+				className="w-full"
+			>
+				<input
+					type="text"
+					placeholder={router.asPath}
+					className="w-full px-2 py-1 text-center placeholder-white placeholder-opacity-75 bg-black bg-opacity-50 rounded text-skin-text"
+					name="search"
+					onChange={(event) => setSearch(event.target.value)}
+					value={search}
+					onFocus={() => setVisibility(true)}
+					onBlur={() => setVisibility(false)}
+				/>
+			</form>
+			{visibility && (
+				<div className="absolute z-50 mt-10 overflow-hidden overflow-y-scroll border border-gray-300 rounded w-max bg-skin-base max-height-48">
+					<ul className="py-1">
+						{links.map((link) => (
+							<li className="px-3 py-2 text-opacity-50 cursor-pointer hover:text-opacity-100">
+								<Link href={link.href}>{link.name}</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</>
+	);
+}
+
+export default SearchBar;
--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,51 +1,24 @@
-import React from "react";
 import Link from "next/link";
+import SearchBar from "./SearchBar";
 import UserDropdown from "./UserDropdown";
-import { LinkListNode } from "@/types/LinkListNode";
 
-const menu = [{ name: "users", href: "/users" }];
-
-function StatusBar({ breadcrumbs }: { breadcrumbs?: LinkListNode[] }) {
+function StatusBar() {
 	return (
-		<nav className="flex items-center w-full px-4 py-2 mx-auto align-middle max-w-7xl">
+		<nav className="flex items-center p-4 mx-auto space-x-4 align-middle max-w-7xl">
 			<Link href="/">
-				<button>
+				<a>
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 -200 960 960"
-						className="w-8 h-8 m-3 fill-current"
-						// alt="42"
+						className="w-8 h-8 fill-current"
 					>
 						<path d="M32 412.6h330.1V578h164.7V279.1H197.3L526.8-51.1H362.1L32 279.1zM597.9 114.2L762.7-51.1H597.9zM762.7 114.2L597.9 279.1v164.8h164.8V279.1L928 114.2V-51.1H762.7z" />
 						<path d="M928 279.1L762.7 443.9H928z" />
 					</svg>
-				</button>
+				</a>
 			</Link>
 			<div className="flex flex-grow">
-				{/* {breadcrumbs &&
-					breadcrumbs.map((item, i ) => (arr
-						<span key={i}>
-							{i !== 0 && (
-								<span className="ml-2 opacity-75">/</span>
-							)}
-							<Link href={item.href}>
-								<a
-									className={`ml-2 ${
-										item.isActive // arr.length - 1 === i
-											? "font-bold"
-											: "font-medium"
-									}`}
-								>
-									{item.name}
-								</a>
-							</Link>
-						</span>
-					))} */}
-				{menu.map((item) => (
-					<Link href={item.href} key={item.href}>
-						<a className="ml-2 font-medium">{item.name}</a>
-					</Link>
-				))}
+				<SearchBar />
 			</div>
 			<UserDropdown />
 		</nav>
