bf67f3137cdb01e81c07788c5fe093c52df395ba
leeoocca <leeoocca@gmail.com>
2021-05-12 02:50:11 +0200

    card: background color/image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,33 @@
 function Card({
 	children,
 	className,
+	backgroundImageURL,
+	backgroundColor,
 }: {
 	children: React.ReactNode;
 	className?: string;
+	backgroundImageURL?: string;
+	backgroundColor?: string;
 }) {
 	return (
 		<div
 			className={
 				className +
-				" " +
-				"p-2 border border-white border-opacity-25 rounded hover:border-opacity-50"
+				" border border-white border-opacity-25 rounded hover:border-opacity-50 relative group"
 			}
+			style={{
+				backgroundColor: backgroundColor || "",
+			}}
 		>
-			{children}
+			{backgroundImageURL && (
+				<>
+					<img
+						src={backgroundImageURL}
+						className="absolute inset-0 object-cover w-full h-full opacity-70 group-hover:opacity-80"
+					/>
+				</>
+			)}
+			<div className="relative flex h-full p-2">{children}</div>
 		</div>
 	);
 }
