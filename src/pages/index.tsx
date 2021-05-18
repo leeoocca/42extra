f258cfacdaa4b6bf0bc7d5054425284f521a3e80
leeoocca <leeoocca@gmail.com>
2021-05-18 03:22:12 +0200

    add homepage links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import UserCard from "@/components/UserCard";
 import { useSession } from "next-auth/client";
+import Link from "next/link";
+
+const links = [
+	{ name: "Users", href: "/users" },
+	{ name: "Campuses", href: "/campus" },
+	{ name: "Cursuses", href: "/cursus" },
+	{ name: "Coalitions", href: "/coalitions" },
+	{ name: "Apps", href: "/apps" },
+];
 
 function Home() {
 	const [session] = useSession();
@@ -8,6 +17,16 @@ function Home() {
 		<>
 			<p>Welcome {session.user?.name}!</p>
 			<UserCard id={session.user.login} />
+			<h2>Start surfing</h2>
+			<ul className="list-disc list-inside">
+				{links.map((link) => (
+					<li key={link.href}>
+						<Link href={link.href}>
+							<a>{link.name}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</>
 	);
 }
