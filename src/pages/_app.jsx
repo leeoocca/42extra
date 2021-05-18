d5e284d660b32ebb74da937e4384bedbe85252f2
leeoocca <leeoocca@gmail.com>
2021-05-18 03:37:35 +0200

    add progress bar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,22 @@ import "@/styles/globals.css";
 import { Provider } from "next-auth/client";
 import fetcherWithToken from "@/lib/fetcherWithToken";
 import { getLayout as getSimpleLayout } from "@/components/layouts/SimpleLayout";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+	size: 2,
+	color: "#22D3EE", // use css variable
+	className: "bar-of-progress",
+	delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", () => {
+	progress.finish();
+	window.scrollTo(0, 0);
+});
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
 	const getLayout = Component.getLayout || getSimpleLayout;
