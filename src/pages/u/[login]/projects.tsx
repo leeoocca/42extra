fd79a4b58ba624a5af344303c6d9a8cda9552376
leeoocca <leeoocca@gmail.com>
2021-06-23 01:52:34 +0200

    cursus sections for user projects

--- src/pages/u/[login]/projects.tsx
+++ src/pages/u/[login]/projects.tsx
@@ -3,18 +3,19 @@ import useAPI from "@/lib/useAPI";
 import { getLayout } from "@/layouts/UserLayout";
 import Link from "next/link";
 import Card from "@/components/Card";
-import { ProjectsUser } from "@/types/User";
+import { User } from "@/types/User";
 import getTimeAgo from "@/lib/getTimeAgo";
 import CardGrid from "@/components/CardGrid";
 
 function UserProjects() {
-	const router = useRouter();
-	const { login } = router.query;
+	const { query } = useRouter();
+	const { login } = query;
 
-	const { data: user, isLoading, isError } = useAPI(`/v2/users/${login}`);
+	const { data: user, isLoading }: { data: User; isLoading: boolean } =
+		useAPI(`/v2/users/${login}`);
 
 	if (isLoading) return <>Loading...</>;
-	if (isError) return <>Error</>;
+	if (!user) return <>Error</>;
 
 	if (!user.projects_users.length)
 		return (
@@ -23,83 +24,155 @@ function UserProjects() {
 			</>
 		);
 
-	return (
-		<>
-			<CardGrid>
-				{user.projects_users.map((project: ProjectsUser) => {
-					const options: Intl.DateTimeFormatOptions = {
-						year: "numeric",
-						month: "2-digit",
-						day: "2-digit",
-						hour: "numeric",
-						minute: "numeric",
-						hour12: false,
-						// dateStyle: "medium",
-						// timeStyle: "short",
-					};
-					let date = null;
-					let timeAgo = null;
-					if (project.marked_at) {
-						const locale = navigator.language || "en";
-						date = new Date(project.marked_at).toLocaleString(
-							locale,
-							options
-						);
-						timeAgo = getTimeAgo(project.marked_at);
-					}
-					return (
-						<Link
-							key={project.id}
-							href={`/p/${project.project.slug}/${login}`}
+	return user.cursus_users
+		.sort(
+			(a, b) =>
+				new Date(b.begin_at).valueOf() - new Date(a.begin_at).valueOf()
+		)
+		.map((cursus) => (
+			<article
+				key={cursus.cursus_id}
+				className="mb-8"
+				id={cursus.cursus_id.toString()}
+			>
+				<header className="mb-4">
+					<div className="flex items-baseline space-x-2">
+						<h2 className="text-2xl font-bold tracking-tight">
+							<Link href={`/cursus/${cursus.cursus_id}`}>
+								<a>{cursus.cursus.name}</a>
+							</Link>
+						</h2>
+						<span className="flex space-x-2 text-sm">
+							<abbr title="level" className="tabular-nums">
+								{cursus.level.toFixed(2)}
+							</abbr>
+							{cursus.grade && (
+								<span className="font-semibold uppercase opacity-75">
+									{cursus.grade}
+								</span>
+							)}
+						</span>
+					</div>
+					<div className="text-sm leading-6">
+						<time
+							dateTime={cursus.begin_at.toString()}
+							title={cursus.begin_at.toString()}
 						>
-							<a>
-								<Card key={project.id}>
-									<div className="flex items-end justify-between w-full h-24">
-										<div>
-											<h2 className="text-lg">
-												{project.project.name}
-											</h2>
-											<p className="text-sm">
+							started {getTimeAgo(cursus.begin_at)}
+						</time>
+						{cursus.end_at && (
+							<>
+								{" · "}
+								<time
+									dateTime={cursus.end_at.toString()}
+									title={cursus.end_at.toString()}
+								>
+									ended {getTimeAgo(cursus.end_at)}
+								</time>
+							</>
+						)}
+						{cursus.blackholed_at && (
+							<>
+								{" · "}
+								<abbr title="Black Hole">BH</abbr> in{" "}
+								<time
+									dateTime={cursus.blackholed_at.toString()}
+									title={cursus.blackholed_at.toString()}
+								>
+									{(
+										(new Date(
+											cursus.blackholed_at
+										).valueOf() -
+											new Date().valueOf()) /
+										1000 /
+										60 /
+										60 /
+										24
+									).toFixed(0)}{" "}
+									days
+								</time>
+							</>
+						)}
+					</div>
+				</header>
+				<main>
+					<CardGrid>
+						{user.projects_users.map((project) => {
+							if (!project.cursus_ids.includes(cursus.cursus_id))
+								return;
+							const options: Intl.DateTimeFormatOptions = {
+								year: "numeric",
+								month: "2-digit",
+								day: "2-digit",
+								hour: "numeric",
+								minute: "numeric",
+								hour12: false,
+								// dateStyle: "medium",
+								// timeStyle: "short",
+							};
+							let date = null;
+							let timeAgo = null;
+							if (project.marked_at) {
+								const locale = navigator.language || "en";
+								date = new Date(
+									project.marked_at
+								).toLocaleString(locale, options);
+								timeAgo = getTimeAgo(project.marked_at);
+							}
+							return (
+								<Link
+									key={project.id}
+									href={`/p/${project.project.slug}/${login}`}
+								>
+									<a>
+										<Card key={project.id}>
+											<div className="flex items-end justify-between w-full h-24">
+												<div>
+													<h2 className="text-lg">
+														{project.project.name}
+													</h2>
+													<p className="text-sm">
+														<span
+															className={`${
+																project.status ===
+																	"in_progress" &&
+																"text-yellow-400"
+															}`}
+														>
+															{project.status.replace(
+																/_/g,
+																" "
+															)}
+														</span>
+														{date && (
+															<time
+																title={date}
+																className="ml-1 opacity-75"
+															>
+																{timeAgo}
+															</time>
+														)}
+													</p>
+												</div>
 												<span
 													className={`${
-														project.status ===
-															"in_progress" &&
-														"text-yellow-400"
+														project["validated?"]
+															? "text-green-400"
+															: "text-red-600"
 													}`}
 												>
-													{project.status.replace(
-														"_",
-														" "
-													)}
+													{project.final_mark}
 												</span>
-												{date && (
-													<time
-														title={date}
-														className="ml-1 opacity-75"
-													>
-														{timeAgo}
-													</time>
-												)}
-											</p>
-										</div>
-										<span
-											className={`${
-												project["validated?"]
-													? "text-green-400"
-													: "text-red-600"
-											}`}
-										>
-											{project.final_mark}
-										</span>
-									</div>
-								</Card>
-							</a>
-						</Link>
-					);
-				})}
-			</CardGrid>
-		</>
-	);
+											</div>
+										</Card>
+									</a>
+								</Link>
+							);
+						})}
+					</CardGrid>
+				</main>
+			</article>
+		));
 }
 
 UserProjects.getLayout = getLayout;
