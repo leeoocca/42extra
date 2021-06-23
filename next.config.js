a3bd7670d67d5270b2c0c5be580e5de3978b72aa
leeoocca <leeoocca@gmail.com>
2021-06-23 02:04:59 +0200

    remove redirects

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ module.exports = {
 	images: {
 		domains: ["cdn.intra.42.fr"],
 	},
-	async redirects() {
-		return [
-			{
-				source: "/u",
-				destination: "/users",
-				permanent: true,
-			},
-		];
-	},
 };
