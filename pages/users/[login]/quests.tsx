535fe15ce3c500827b9001d74b85e69a4111367d
leeoocca <leeoocca@gmail.com>
2021-11-10 15:36:07 +0100

    feat: group user quests by validation status

--- pages/users/[login]/quests.tsx
+++ pages/users/[login]/quests.tsx
@@ -4,6 +4,28 @@ import { getLayout } from "ui/layouts/UserLayout";
 import CardGrid from "ui/CardGrid";
 import Card from "ui/Card";
 import getTimeAgo from "lib/getTimeAgo";
+import { Heading } from "theme-ui";
+
+const Quest = ({ quest }) => (
+	<Card key={quest.id}>
+		<div className="flex flex-col h-32 overflow-hidden text-sm place-content-end">
+			<h2 className="text-base font-semibold">{quest.quest.name}</h2>
+			<p>{quest.quest.description}</p>
+			<p>
+				<span className="text-xs uppercase">{quest.quest.kind}</span>
+				{quest.validated_at && (
+					<>
+						{" "}
+						–{" "}
+						<time title={quest.validated_at} className="opacity-75">
+							{getTimeAgo(quest.validated_at)}
+						</time>
+					</>
+				)}
+			</p>
+		</div>
+	</Card>
+);
 
 function UserQuests() {
 	const router = useRouter();
@@ -25,36 +47,25 @@ function UserQuests() {
 			</>
 		);
 
+	const validated = quests.filter((quest) => quest.validated_at);
+	const not_validated = quests.filter((quest) => !quest.validated_at);
+
 	return (
-		<CardGrid>
-			{quests.map((quest) => (
-				<Card key={quest.id}>
-					<div className="flex flex-col h-32 overflow-hidden text-sm place-content-end">
-						<h2 className="text-base font-semibold">
-							{quest.quest.name}
-						</h2>
-						<p>{quest.quest.description}</p>
-						<p>
-							<span className="text-xs uppercase">
-								{quest.quest.kind}
-							</span>
-							{quest.validated_at && (
-								<>
-									{" "}
-									–{" "}
-									<time
-										title={quest.validated_at}
-										className="opacity-75"
-									>
-										{getTimeAgo(quest.validated_at)}
-									</time>
-								</>
-							)}
-						</p>
-					</div>
-				</Card>
-			))}
-		</CardGrid>
+		<>
+			<CardGrid>
+				{validated.map((quest) => (
+					<Quest quest={quest} />
+				))}
+			</CardGrid>
+			<Heading mb={2} mt={4}>
+				Not validated yet
+			</Heading>
+			<CardGrid>
+				{not_validated.map((quest) => (
+					<Quest quest={quest} />
+				))}
+			</CardGrid>
+		</>
 	);
 }
 
