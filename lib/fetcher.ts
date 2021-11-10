d9327bbf694074ad5cb1e5b62c210413cee7f691
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-10 17:31:19 +0100

    feat: limit requests to 1 every 505 ms

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,10 @@
-const fetcher = (url: string) => fetch(url).then((r) => r.json());
+import { RateLimiter } from "limiter";
+
+const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 505 });
+
+async function fetcher(url: string) {
+	await limiter.removeTokens(1);
+	return fetch(url).then((r) => r.json());
+}
 
 export default fetcher;
