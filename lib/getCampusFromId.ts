24ab46e246548c1972bf9614c900a0b57ed98dd4
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-10 20:19:16 +0100

    fix: import of uncommittable file

--- lib/getCampusFromId.ts
+++ lib/getCampusFromId.ts
@@ -1,4 +1,4 @@
-import campuses from "lib/campuses";
+// import campuses from "lib/campuses";
 
 // import { Campus } from "types/User";
 // import useAPI from "lib/useAPI";
@@ -7,7 +7,7 @@ export default async function getCampusCityFromId(campus_id: Number) {
 	// const { data: campuses }: { data: Campus[] } = await useAPI(
 	// 	"/v2/campus?page[size]=100"
 	// );
-	const campus = campuses.find((campus) => campus.id === campus_id);
-	if (campus) return campus;
+	// const campus = campuses.find((campus) => campus.id === campus_id);
+	// if (campus) return campus;
 	return null;
 }
