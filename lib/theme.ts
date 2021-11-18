cea9325255e07b2ca9527593106175ebc1315ec9
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-18 22:39:23 +0100

    feat: add missing stuff to theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -16,6 +16,8 @@ const theme: Theme = {
 		heading: "inherit",
 		monospace: "'JetBrains Mono', ui-monospace, Menlo, Monaco, monospace",
 	},
+	breakpoints: ["40em", "52em", "64em"],
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
 	fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
 	fontWeights: {
 		body: 400,
