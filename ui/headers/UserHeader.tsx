7b35e9f92963e1d7819427c1c8b2500fe710b063
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-18 22:33:31 +0100

    feat: stack user header on mobile

--- ui/headers/UserHeader.tsx
+++ ui/headers/UserHeader.tsx
@@ -1,8 +1,11 @@
-import Avatar from "ui/Avatar";
+import { useRouter } from "next/router";
+
+import { Flex, Box, Heading } from "@theme-ui/components";
 import { BadgeCheckIcon } from "@heroicons/react/outline";
+
 import { User } from "types/User";
 import useAPI from "lib/useAPI";
-import { useRouter } from "next/router";
+import Avatar from "ui/Avatar";
 
 function getCustomUserLogin(user: User): string {
 	const selectedTitle = user.titles_users.find((title) => title.selected);
@@ -31,48 +34,65 @@ function UserHeader() {
 
 	return (
 		<header className="flex flex-row items-center px-4 py-2 mx-auto my-6 max-w-7xl">
-			<Avatar
-				url={
-					isError
-						? "https://cdn.intra.42.fr/users/3b3.jpg"
-						: user
-						? user.image_url
-						: null
-				}
-				size={128}
-				className="w-16 h-16 md:w-32 md:h-32"
-			/>
-			{!isLoading && (
-				<div className="pb-1 ml-4">
-					<h1 className="inline-flex text-4xl font-bold">
-						{isError ? (
-							"Error"
-						) : (
-							<>
-								<span>
-									{/* maybe just always use user.login? */}
-									{/* useful when loading though */}
-									{customUserLogin
-										? customUserLogin
-										: Number(login) > 0
-										? user.login
-										: login}
-								</span>
-								{user["staff?"] && (
-									<span
-										title={`${login} is a member of the staff`}
-									>
-										<BadgeCheckIcon className="mt-2 ml-1.5 w-7" />
+			<Flex
+				sx={{
+					flexDirection: ["column", , "row"],
+					alignItems: "center",
+					gap: [2, , 3],
+					width: "100%",
+				}}
+			>
+				<Box sx={{ w: 6, h: 6 }}>
+					<Avatar
+						url={
+							isError
+								? "https://cdn.intra.42.fr/users/3b3.jpg"
+								: user
+								? user.image_url
+								: null
+						}
+						size={128}
+					/>
+				</Box>
+				{!isLoading && (
+					<Box sx={{ textAlign: ["center", , "left"] }}>
+						<Heading
+							as="h1"
+							sx={{
+								fontSize: 36,
+								display: "flex",
+								lineHeight: "2.5rem",
+							}}
+						>
+							{isError ? (
+								"Error"
+							) : (
+								<>
+									<span>
+										{/* maybe just always use user.login? */}
+										{/* useful when loading though */}
+										{customUserLogin
+											? customUserLogin
+											: Number(login) > 0
+											? user.login
+											: login}
 									</span>
-								)}{" "}
-							</>
-						)}
-					</h1>
-					<p className="text-3xl font-medium">
-						{isError ? "Don't panic!" : user.usual_full_name}
-					</p>
-				</div>
-			)}
+									{user["staff?"] && (
+										<span
+											title={`${login} is a member of the staff`}
+										>
+											<BadgeCheckIcon className="mt-2 ml-1.5 w-7" />
+										</span>
+									)}{" "}
+								</>
+							)}
+						</Heading>
+						<p className="text-3xl font-medium">
+							{isError ? "Don't panic!" : user.usual_full_name}
+						</p>
+					</Box>
+				)}
+			</Flex>
 		</header>
 	);
 }
