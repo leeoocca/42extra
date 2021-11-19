e6774838854bb105598921d4fda6e5141de1a7fe
leeoocca <leeoocca@gmail.com>
2021-11-20 00:18:40 +0100

    refactor: move kbar to own file
    also
    - refactor _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,16 @@
 import "styles/globals.css";
-import { CSSProperties, useMemo } from "react";
 import Router from "next/router";
 
-import {
-	KBarAnimator,
-	KBarPortal,
-	KBarPositioner,
-	KBarProvider,
-	KBarResults,
-	KBarSearch,
-	useMatches,
-} from "kbar";
-import { NO_GROUP } from "kbar/lib/useMatches";
+import { KBarProvider } from "kbar";
 import { SWRConfig } from "swr";
-import { ThemeProvider, Text, Flex } from "theme-ui";
+import { ThemeProvider } from "theme-ui";
 import ProgressBar from "@badrap/bar-of-progress";
-import { Provider as SessionProvider, signOut } from "next-auth/client";
+import { Provider as SessionProvider } from "next-auth/client";
 
 import theme from "lib/theme";
 import fetcher from "lib/fetcher";
 import { globalActions } from "lib/actions";
+import CommandBar from "ui/CommandBar";
 import { getLayout as getSimpleLayout } from "ui/layouts/SimpleLayout";
 
 const progress = new ProgressBar({
@@ -36,137 +27,33 @@ Router.events.on("routeChangeComplete", () => {
 });
 Router.events.on("routeChangeError", progress.finish);
 
-const searchStyle: CSSProperties = {
-	padding: "12px 16px",
-	fontSize: "16px",
-	width: "100%",
-	boxSizing: "border-box",
-	outline: "none",
-	border: "none",
-	background: "var(--theme-ui-colors-background)",
-	color: "var(--theme-ui-colors-text)",
+const sessionOptions = {
+	clientMaxAge: 60 * 60,
 };
 
-const animatorStyle: CSSProperties = {
-	maxWidth: "500px",
-	width: "100%",
-	background: "var(--theme-ui-colors-background)",
-	color: "var(--theme-ui-colors-text)",
-	borderRadius: "8px",
-	overflow: "hidden",
-	boxShadow: "var(--shadow)",
+const SWRSettings = {
+	fetcher: fetcher,
+	onError: (err) => {
+		console.error(err);
+	},
+	revalidateOnFocus: false,
+	dedupingInterval: 60 * 1000,
+	errorRetryInterval: 2000,
 };
 
 export default function MyApp({ Component, pageProps }) {
 	const getLayout = Component.getLayout || getSimpleLayout;
 
 	return (
-		<SessionProvider
-			session={pageProps.session}
-			options={{
-				clientMaxAge: 60 * 60,
-			}}
-		>
-			<SWRConfig
-				value={{
-					fetcher: fetcher,
-					onError: (err) => {
-						console.error(err);
-					},
-					revalidateOnFocus: false,
-					dedupingInterval: 60 * 1000,
-					errorRetryInterval: 2000,
-				}}
-			>
+		<SessionProvider session={pageProps.session} options={sessionOptions}>
+			<SWRConfig value={SWRSettings}>
 				<ThemeProvider theme={theme}>
 					<KBarProvider actions={globalActions}>
-						<KBarPortal>
-							<KBarPositioner>
-								<KBarAnimator style={animatorStyle}>
-									<KBarSearch style={searchStyle} />
-									<RenderResults />
-								</KBarAnimator>
-							</KBarPositioner>
-						</KBarPortal>
+						<CommandBar />
 						{getLayout(<Component {...pageProps} />)}
 					</KBarProvider>
 				</ThemeProvider>
 			</SWRConfig>
 		</SessionProvider>
-	);
-}
-
-function RenderResults() {
-	const groups = useMatches();
-	const flattened = useMemo(
-		() =>
-			groups.reduce((acc, curr) => {
-				acc.push(curr.name);
-				acc.push(...curr.actions);
-				return acc;
-			}, []),
-		[groups]
-	);
-
-	return (
-		<KBarResults
-			items={flattened.filter((i) => i !== NO_GROUP)}
-			onRender={({ item, active }) =>
-				typeof item === "string" ? (
-					<Text
-						sx={{
-							textTransform: "uppercase",
-							padding: "8px 16px",
-							fontSize: "10px",
-							opacity: 0.5,
-						}}
-					>
-						{item}
-					</Text>
-				) : (
-					<Flex
-						sx={{
-							backgroundColor: active ? "muted" : "transparent",
-							// maxHeight: 400,
-							// overflow: "auto",
-							justifyContent: "space-between",
-							padding: "12px 16px",
-							borderLeft: "2px solid",
-							borderLeftColor: active ? "text" : "transparent",
-							alignItems: "center",
-							cursor: "pointer",
-						}}
-					>
-						<Text>{item.name}</Text>
-						{item.shortcut?.length ? (
-							<div
-								aria-hidden
-								style={{
-									display: "grid",
-									gridAutoFlow: "column",
-									gap: "4px",
-								}}
-							>
-								{item.shortcut.map((sc) => (
-									<Text
-										key={sc}
-										as="kbd"
-										sx={{
-											padding: "4px 6px",
-											background:
-												"rgba(255 255 255 / .1)",
-											borderRadius: "4px",
-											fontSize: 14,
-										}}
-									>
-										{sc}
-									</Text>
-								))}
-							</div>
-						) : null}
-					</Flex>
-				)
-			}
-		/>
 	);
 }
---
+++ ui/CommandBar.tsx
@@ -0,0 +1,121 @@
+import { CSSProperties, useMemo } from "react";
+
+import {
+	KBarAnimator,
+	KBarPortal,
+	KBarPositioner,
+	KBarResults,
+	KBarSearch,
+	useMatches,
+} from "kbar";
+import { NO_GROUP } from "kbar/lib/useMatches";
+import { Flex, Text } from "@theme-ui/components";
+
+const searchStyle: CSSProperties = {
+	padding: "12px 16px",
+	fontSize: "16px",
+	width: "100%",
+	boxSizing: "border-box",
+	outline: "none",
+	border: "none",
+	background: "var(--theme-ui-colors-background)",
+	color: "var(--theme-ui-colors-text)",
+};
+
+const animatorStyle: CSSProperties = {
+	maxWidth: "500px",
+	width: "100%",
+	background: "var(--theme-ui-colors-background)",
+	color: "var(--theme-ui-colors-text)",
+	borderRadius: "8px",
+	overflow: "hidden",
+	boxShadow: "var(--shadow)",
+};
+
+export default function CommandBar() {
+	return (
+		<KBarPortal>
+			<KBarPositioner>
+				<KBarAnimator style={animatorStyle}>
+					<KBarSearch style={searchStyle} />
+					<RenderResults />
+				</KBarAnimator>
+			</KBarPositioner>
+		</KBarPortal>
+	);
+}
+
+function RenderResults() {
+	const groups = useMatches();
+	const flattened = useMemo(
+		() =>
+			groups.reduce((acc, curr) => {
+				acc.push(curr.name);
+				acc.push(...curr.actions);
+				return acc;
+			}, []),
+		[groups]
+	);
+
+	return (
+		<KBarResults
+			items={flattened.filter((i) => i !== NO_GROUP)}
+			onRender={({ item, active }) =>
+				typeof item === "string" ? (
+					<Text
+						sx={{
+							textTransform: "uppercase",
+							padding: "8px 16px",
+							fontSize: "10px",
+							opacity: 0.5,
+						}}
+					>
+						{item}
+					</Text>
+				) : (
+					<Flex
+						sx={{
+							backgroundColor: active ? "muted" : "transparent",
+							// maxHeight: 400,
+							// overflow: "auto",
+							justifyContent: "space-between",
+							padding: "12px 16px",
+							borderLeft: "2px solid",
+							borderLeftColor: active ? "text" : "transparent",
+							alignItems: "center",
+							cursor: "pointer",
+						}}
+					>
+						<Text>{item.name}</Text>
+						{item.shortcut?.length ? (
+							<div
+								aria-hidden
+								style={{
+									display: "grid",
+									gridAutoFlow: "column",
+									gap: "4px",
+								}}
+							>
+								{item.shortcut.map((sc) => (
+									<Text
+										key={sc}
+										as="kbd"
+										sx={{
+											padding: "4px 6px",
+											background:
+												"rgba(255 255 255 / .1)",
+											borderRadius: "4px",
+											fontSize: 14,
+										}}
+									>
+										{sc}
+									</Text>
+								))}
+							</div>
+						) : null}
+					</Flex>
+				)
+			}
+		/>
+	);
+}
