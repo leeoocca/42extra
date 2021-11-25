a28f09bdb8857ec1b03d9e0af2e7d75b9446095d
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-25 23:23:28 +0100

    fix(types): fetcher extending Error
    now using simple object

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -13,10 +13,7 @@ export default async function fetcher(url: string) {
 	});
 
 	if (!res.ok) {
-		const error = new Error("An error occurred while fetching the data.");
-		error.desc = res.statusText;
-		error.status = res.status;
-		throw error;
+		throw { desc: res.statusText, status: res.status };
 	}
 
 	return await res.json();
