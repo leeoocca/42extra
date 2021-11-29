83e73829c565bb632765b5fa039993fe48353f99
leeoocca <leeoocca@gmail.com>
2021-11-29 06:33:03 +0100

    feat: better /query
    - responsive styling
    - fix loading state with empty query
    - use themed.pre

--- pages/tools/query.tsx
+++ pages/tools/query.tsx
@@ -12,6 +12,7 @@ import {
 	Text,
 } from "@theme-ui/components";
 import { AlertOctagon } from "react-feather";
+import { Themed } from "@theme-ui/mdx";
 
 export default function Query() {
 	const { data: session } = useSession();
@@ -22,9 +23,9 @@ export default function Query() {
 
 	async function handleSubmit(e) {
 		e.preventDefault();
-		setData(null);
-		setError(null);
 		if (query.length) {
+			setData(null);
+			setError(null);
 			const res = await fetch(`/api/v2/${query}`, {
 				headers: { Authorization: `Bearer ${session.accessToken}` },
 			});
@@ -37,39 +38,47 @@ export default function Query() {
 	return (
 		<>
 			<Box as="form" onSubmit={handleSubmit} mb={3}>
-				<Flex sx={{ alignItems: "baseline" }}>
-					<Text sx={{ width: "auto", opacity: "75%" }}>
-						https://api.intra.42.fr/v2/
-					</Text>
-					<Input
-						name="query"
-						type="text"
-						value={query}
-						placeholder="me"
-						onChange={(e) => setQuery(e.target.value)}
-						sx={{
-							width: "auto",
-							flexGrow: 1,
-							border: "none",
-							borderBottom: "1px solid gray",
-							borderRadius: 0,
-						}}
-					/>
+				<Grid columns={[1, , "6fr 1fr"]}>
+					<Flex sx={{ alignItems: "baseline" }}>
+						<Text
+							as="small"
+							sx={{
+								width: "auto",
+								opacity: "75%",
+							}}
+						>
+							https://api.intra.42.fr/v2/
+						</Text>
+						<Input
+							name="query"
+							type="text"
+							value={query}
+							placeholder="me"
+							onChange={(e) => setQuery(e.target.value)}
+							sx={{
+								width: "auto",
+								flexGrow: 1,
+								border: "none",
+								borderBottom: "1px solid gray",
+								borderRadius: 0,
+							}}
+						/>
+					</Flex>
 					<Button type="submit">Send</Button>
-				</Flex>
+				</Grid>
 			</Box>
 			{error && (
 				<Alert bg="purple">
 					{error.status} - {error.statusText}
 				</Alert>
 			)}
-			<Box as="pre" sx={{ overflow: "auto" }}>
+			<Themed.pre>
 				{data === undefined
 					? "Try `me`"
 					: !data
 					? !error && "loading..."
 					: JSON.stringify(data, null, "\t")}
-			</Box>
+			</Themed.pre>
 		</>
 	);
 }
