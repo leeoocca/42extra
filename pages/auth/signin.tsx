d92ae4af1c477d8c67c20d3b836f985df75cfd6f
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-30 19:59:24 +0100

    feat: don't allow signin if user is authenticated

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,13 +1,32 @@
-import { Button, Grid } from "@theme-ui/components";
-import { signIn } from "next-auth/react";
+import Link from "next/link";
+
+import { Button, Grid, Text, Link as ThemeLink } from "@theme-ui/components";
+import { signIn, useSession } from "next-auth/react";
+
 import Logo from "ui/Logo";
+import { useRouter } from "next/router";
 
 export default function SignIn() {
+	const { status } = useSession();
+
 	const router = useRouter();
 	const { callbackUrl } = router.query;
 
+	if (status === "loading") return null;
+
 	return (
 		<Grid sx={{ placeItems: "center", minHeight: "calc(100vh - 2rem)" }}>
+			{status === "authenticated" ? (
+				<Text sx={{ textAlign: "center", lineHeight: 2 }}>
+					Already signed in.
+					<br />
+					<Link href="/" passHref>
+						<ThemeLink sx={{ cursor: "pointer" }}>
+							Go home
+						</ThemeLink>
+					</Link>
+				</Text>
+			) : (
 				<Button
 					onClick={() =>
 						signIn("42", { callbackUrl: String(callbackUrl) })
@@ -24,6 +43,7 @@ export default function SignIn() {
 				>
 					Sign in with <Logo width={24} />
 				</Button>
+			)}
 		</Grid>
 	);
 }
