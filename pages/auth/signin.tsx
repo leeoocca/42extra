f363173d22b5ce0e807e7eb610f285bf99afcafc
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-30 19:56:28 +0100

    fix: signin callbackUrl not redirecting

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -2,15 +2,20 @@ import { Button, Grid } from "@theme-ui/components";
 import { signIn } from "next-auth/react";
 
 export default function SignIn() {
+	const router = useRouter();
+	const { callbackUrl } = router.query;
+
 	return (
 		<Grid sx={{ placeItems: "center", minHeight: "calc(100vh - 2rem)" }}>
-			<Button
-				onClick={() => signIn("42")}
-				sx={{
-					bg: "transparent",
-					border: "1px solid gray",
-				}}
-			>
+				<Button
+					onClick={() =>
+						signIn("42", { callbackUrl: String(callbackUrl) })
+					}
+					sx={{
+						bg: "transparent",
+						border: "1px solid gray",
+					}}
+				>
 				Sign in with 42
 			</Button>
 		</Grid>
