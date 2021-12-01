54b16d872c13e517b0441efe124febe28150e120
leeoocca <leeoocca@gmail.com>
2021-12-01 16:31:13 +0100

    feat: favicons with new logo
    - reduce assets to one vector and two raster images
    - still support iOS and Mac Safari favicons
    - fix apple-mobile-web-app detection
    - update colors in head and webmanifest

--- ui/Favicons.tsx
+++ ui/Favicons.tsx
@@ -1,45 +1,24 @@
 import Head from "next/head";
 
-import { useThemeUI } from "theme-ui";
-
 export default function Favicons() {
-	const { theme } = useThemeUI();
 	return (
 		<Head>
 			<title>42extra</title>
-
-			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-			<link rel="manifest" href="/site.webmanifest" />
-
-			<meta
-				name="theme-color"
-				content={theme.colors.background.toString()}
+			<link rel="icon" type="image/svg+xml" href="/icon.svg" />
+			<link
+				rel="icon"
+				type="image/png"
+				href="/icon_x192.png"
+				sizes="any"
 			/>
+			<link rel="apple-touch-icon" href="/icon_x192.png" />
+			<link rel="mask-icon" href="/icon.svg" color="#121212" />
+			<meta name="theme-color" content="#121212" />
+			<link rel="manifest" href="/site.webmanifest" />
+			<meta name="apple-mobile-web-app-capable" content="yes" />
 			<meta
 				name="apple-mobile-web-app-status-bar-style"
-				content="black-translucent"
-			/>
-			<link
-				rel="mask-icon"
-				href="/safari-pinned-tab.svg"
-				color="#01babc"
-			/>
-			<link
-				rel="apple-touch-icon"
-				sizes="180x180"
-				href="/apple-touch-icon.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="32x32"
-				href="/favicon-32x32.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="16x16"
-				href="/favicon-16x16.png"
+				content="black"
 			/>
 		</Head>
 	);
