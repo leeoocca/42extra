975f62018af206c33facff4d31a41d0bbd8b9593
leeoocca <leeoocca@gmail.com>
2021-12-03 08:00:43 +0100

    fix: handle missing image in achievement card

--- ui/AchievementCard.tsx
+++ ui/AchievementCard.tsx
@@ -6,13 +6,16 @@ import Card from "./Card";
 export default function AchievementCard({ a }: { a: Achievement }) {
 	return (
 		<Card className="items-center">
-			<Image
-				src={
-					"https://cdn.intra.42.fr" + a.image.replace("/uploads", "")
-				}
-				width={48}
-				height={48}
-			/>
+			{a.image && (
+				<Image
+					src={`https://cdn.intra.42.fr${a.image.replace(
+						"/uploads",
+						""
+					)}`}
+					width={48}
+					height={48}
+				/>
+			)}
 			<div className="flex flex-col justify-center ml-2 leading-relaxed pt-0.5 pb-1.5">
 				<h3 className="font-bold tracking-tight">{a.name}</h3>
 				<p className="text-sm">{a.description}</p>
