bedee7f2e3a66a701d0ad4f08ab536d53255758d
leeoocca <leeoocca@gmail.com>
2021-12-03 02:58:43 +0100

    refactor: simplify UsersIndex

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,63 +1,10 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-// import useAPI from "lib/useAPI";
-// import { User } from "types/User";
-// import { useSession } from "next-auth/react";
+import { Themed } from "@theme-ui/mdx";
 
 function UsersIndex() {
-	const [login, setLogin] = useState("");
-	// const { data: session } = useSession();
-
-	// const {
-	// 	data,
-	// 	isLoading,
-	// 	isError,
-	// }: { data: User[]; isLoading: boolean; isError: any } = useAPI(
-	// 	`/v2/users?filter[primary_campus_id]=30`
-	// );
-
-	const router = useRouter();
-
 	return (
 		<>
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					router.push(`/users/${login}`, null, { shallow: true });
-				}}
-			>
-				<input
-					type="search"
-					placeholder="login"
-					className="text-black"
-					name="login"
-					onChange={(event) => setLogin(event.target.value)}
-					value={login}
-				/>
-				<button type="submit">Search</button>
-			</form>
-			{/* <ul>
-				{!isLoading &&
-					!isError &&
-					data.map((user) => (
-						<li key={user.id} className="flex py-4">
-							<img
-								className="w-10 h-10 rounded-full"
-								src={`https://cdn.intra.42.fr/users/${user.login}.jpeg`}
-								alt=""
-							/>
-							<div className="ml-3">
-								<p className="text-sm font-medium text-gray-200">
-									{user.login}
-								</p>
-								<p className="text-sm text-gray-500">
-									{user.email}
-									{console.log(user)}
-								</p>
-							</div>
-						</li>
-					))}
-			</ul> */}
+			Toggle the Command Bar and select <Themed.code>Users</Themed.code>{" "}
+			to go to an user&apos;s profile.
 		</>
 	);
 }
