701122e26d90138a7fbe0465cdbf9a62e751b853
Leonardo Rocca <lrocca@student.42roma.it>
2021-12-05 01:16:30 +0100

    feat: header color from theme-ui primary

--- ui/headers/Header.tsx
+++ ui/headers/Header.tsx
@@ -4,9 +4,23 @@ export default function Header({ children }) {
 	return (
 		<Box
 			sx={{
-				backgroundColor: "rgba(var(--nav), var(--nav-opacity))",
-				transition:
-					"background-color 500ms cubic-bezier(0.4, 0, 0.2, 1)",
+				// bg: "primarydim",
+				// backgroundColor:
+				// 	"rgba(var(--theme-ui-colors-primary), var(--nav-opacity))",
+				position: "relative",
+				"&:after": {
+					transition:
+						"background-color 500ms cubic-bezier(0.4, 0, 0.2, 1)",
+					content: "''",
+					width: "100%",
+					height: "100%",
+					backgroundColor: "var(--theme-ui-colors-primary)",
+					opacity: 0.6,
+					position: "absolute",
+					top: 0,
+					left: 0,
+					zIndex: -1,
+				},
 			}}
 		>
 			<Container p={0} as="header" id="header">
