99bf47a0c3b10ac87536c0c2fe7f7843663c93eb
Leonardo Rocca <lrocca@student.42roma.it>
2021-12-10 22:02:40 +0100

    feat: don't scroll into view current navlink item

--- ui/NavLink.tsx
+++ ui/NavLink.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
@@ -6,18 +5,8 @@ import { LinkListNode } from "types/LinkListNode";
 
 function NavLink({ name, href, className }: LinkListNode) {
 	const router = useRouter();
-	const activeNavRef = useRef<HTMLAnchorElement>(null);
 
-	const active = router.asPath === href;
-
-	useEffect(() => {
-		if (active && activeNavRef.current) {
-			activeNavRef.current.scrollIntoView({
-				block: "nearest",
-				inline: "center",
-			});
-		}
-	}, [active]);
+	const active = router.pathname === href;
 
 	return (
 		<Link href={{ pathname: href, query: router.query }}>
@@ -27,7 +16,6 @@ function NavLink({ name, href, className }: LinkListNode) {
 						? "opacity-100 border-white"
 						: "opacity-75 border-transparent"
 				} hover:opacity-100 hover:text-white border-b-2 px-1 py-2 font-medium ease-in-out transition-opacity focus:opacity-100 ${className}`}
-				ref={activeNavRef}
 			>
 				{name}
 			</a>
