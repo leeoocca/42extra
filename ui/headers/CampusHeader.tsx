7f41148cc7dc5deee9475fa765e8cf7580041125
leeoocca <leeoocca@gmail.com>
2021-12-11 05:06:39 +0100

    feat: CampusHeader loading state

--- ui/headers/CampusHeader.tsx
+++ ui/headers/CampusHeader.tsx
@@ -7,24 +7,26 @@ import HeaderNav from "./HeaderNav";
 import { CampusNavLinks } from "lib/NavLinks";
 
 export default function CampusHeader() {
-	const router = useRouter();
-	const { id } = router.query;
+	const {
+		query: { id },
+	} = useRouter();
 
-	const { data: c } = useAPI(`/v2/campus/${id}`);
+	const { data: c = { name: "Loading...", city: null, country: null } } =
+		useAPI(`/v2/campus/${id}`);
 
 	document.documentElement.style.setProperty("--nav", "");
-
-	if (!c) return null;
 
 	return (
 		<>
 			<Box p={3}>
 				<h1 className="text-3xl font-bold leading-relaxed">{c.name}</h1>
 				<Flex sx={{ gap: 3 }}>
-					<Text sx={{ fontFamily: "monosFlexace" }}>#{c.id}</Text>
-					<Text>
-						{c.city}, {c.country}
-					</Text>
+					<Text sx={{ fontFamily: "monosFlexace" }}>#{id}</Text>
+					{c.city && (
+						<Text>
+							{c.city}, {c.country}
+						</Text>
+					)}
 				</Flex>
 			</Box>
 			<HeaderNav navLinks={CampusNavLinks()} />
