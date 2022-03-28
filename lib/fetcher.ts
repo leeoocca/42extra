882701c412456d68ddeaf2ad9ff67bac041cec54
Leonardo Rocca <lrocca@student.42roma.it>
2022-03-29 00:08:49 +0200

    feat: fetcher support for multiple methods

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,20 +1,21 @@
 import { RateLimiter } from "limiter";
 import { getSession } from "next-auth/react";
 
-const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 510 });
+const limiter = new RateLimiter({ tokensPerInterval: 1, interval: 600 });
 
-export default async function fetcher(url: string) {
+export default async function fetcher(url: string, method: string = "GET") {
 	const session = await getSession();
 
 	await limiter.removeTokens(1);
 
 	const res = await fetch(url, {
 		headers: { Authorization: `Bearer ${session.accessToken}` },
+		method: method,
 	});
 
 	if (!res.ok) {
-		throw { desc: res.statusText, status: res.status };
+		throw res;
 	}
 
-	return await res.json();
+	return method === "GET" ? await res.json() : res;
 }
