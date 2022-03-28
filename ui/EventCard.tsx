6c83b8d7ecfa86d84ce966b78796dbf1e0435f45
Leonardo Rocca <lrocca@student.42roma.it>
2022-03-29 00:16:50 +0200

    feat: EventCard link

--- ui/EventCard.tsx
+++ ui/EventCard.tsx
@@ -1,20 +1,25 @@
+import Link from "next/link";
+
 import { Card, Text } from "theme-ui";
 
+import { Event } from "types/42/Event";
 import { locale } from "lib/constants";
 
-export default function EventCard({ event }) {
+export default function EventCard({ event }: { event: Event }) {
 	return (
-		<Card bg="muted" px={3} py={2}>
-			<b>{event.name}</b>
-			<Text as="p" sx={{ fontFamily: "monospace" }}>
-				{event.kind}
-			</Text>
-			<p>
-				{event.nbr_subscribers}
-				{event.max_people && ` / ${event.max_people}`}
-			</p>
-			<p>{event.location}</p>
-			<p>{new Date(event.begin_at).toLocaleString(locale)}</p>
-		</Card>
+		<Link href={`/events/${event.id}`}>
+			<Card bg="muted" px={3} py={2} sx={{ cursor: "pointer" }}>
+				<b>{event.name}</b>
+				<Text as="p" sx={{ fontFamily: "monospace" }}>
+					{event.kind}
+				</Text>
+				<p>
+					{event.nbr_subscribers}
+					{event.max_people && ` / ${event.max_people}`}
+				</p>
+				<p>{event.location}</p>
+				<p>{new Date(event.begin_at).toLocaleString(locale)}</p>
+			</Card>
+		</Link>
 	);
 }
