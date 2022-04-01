9e45f6352c5fa72660f1e5ede560ffa0dd774c7c
leeoocca <leeoocca@gmail.com>
2022-04-01 07:27:46 +0200

    feat: deprecated_id as constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const apiBaseUrl = "https://api.intra.42.fr";
 export const locale = "en-UK";
 export const REPO = "https://github.com/leeoocca/42extra";
+export const TITLE_DEPRECATED_ID = 82;
