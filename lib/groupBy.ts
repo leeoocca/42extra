b7a6b8ee1d623b9314defee309f7d7b1c44e4058
leeoocca <leeoocca@gmail.com>
2022-09-28 02:32:09 +0200

    refactor: support for array of keys in groupBy

--- lib/groupBy.ts
+++ lib/groupBy.ts
@@ -1,15 +1,26 @@
+type NotArray<T> = T;
+
 export default function groupBy<TItem, TKey>(
 	list: TItem[],
-	keyGetter: (arg: TItem) => TKey
+	keyGetter: (arg: TItem) => TKey | TKey[]
 ): { name: TKey; value: TItem[] }[] {
 	const map = new Map();
 	list.forEach((item) => {
 		const key = keyGetter(item);
-		const collection: TItem[] = map.get(key);
-		if (!collection) {
-			map.set(key, [item]);
+
+		function setValueOrPush(singleKey) {
+			const collection: TItem[] = map.get(singleKey);
+			if (!collection) {
+				map.set(singleKey, [item]);
+			} else {
+				collection.push(item);
+			}
+		}
+
+		if (Array.isArray(key)) {
+			key.forEach((singleKey) => setValueOrPush(singleKey));
 		} else {
-			collection.push(item);
+			setValueOrPush(key);
 		}
 	});
 	return Array.from(map, ([name, value]) => ({ name, value }));
